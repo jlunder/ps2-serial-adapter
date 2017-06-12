2f0bac9a1157eb55f47e40abfbc1ffe264ac1631
Joseph Lunderville <jlunder@catfish.dhs.org>
2017-06-12 02:55:29 -0700

    Reverse changes to comments.

--- ps2-serial/src/PS2X.h
+++ ps2-serial/src/PS2X.h
@@ -1,6 +1,6 @@
 /******************************************************************
 *  Super amazing PS2 controller Arduino Library v1.8
-*		details and example sketch:
+*		details and example sketch: 
 *			http://www.billporter.info/?p=240
 *
 *    Original code by Shutter on Arduino Forums
@@ -14,13 +14,13 @@
 *		Kurt Eckhardt
 *
 *  Lib version history
-*    0.1 made into library, added analog stick support.
+*    0.1 made into library, added analog stick support. 
 *    0.2 fixed config_gamepad miss-spelling
 *        added new functions:
 *          NewButtonState();
-*          NewButtonState(uint16_t);
-*          ButtonPressed(uint16_t);
-*          ButtonReleased(uint16_t);
+*          NewButtonState(unsigned int);
+*          ButtonPressed(unsigned int);
+*          ButtonReleased(unsigned int);
 *        removed 'PS' from begining of ever function
 *    1.0 found and fixed bug that wasn't configuring controller
 *        added ability to define pins
@@ -29,14 +29,14 @@
 *        added:
 *          enableRumble();
 *          enablePressures();
-*    1.1
+*    1.1  
 *        added some debug stuff for end user. Reports if no controller found
 *        added auto-increasing sentence delay to see if it helps compatibility.
 *    1.2
-*        found bad math by Shutter for original clock. Was running at 50kHz, not the required 500kHz.
-*        fixed some of the debug reporting.
-*	1.3
-*	    Changed clock back to 50kHz. CuriousInventor says it's suppose to be 500kHz, but doesn't seem to work for everybody.
+*        found bad math by Shutter for original clock. Was running at 50kHz, not the required 500kHz. 
+*        fixed some of the debug reporting. 
+*	1.3 
+*	    Changed clock back to 50kHz. CuriousInventor says it's suppose to be 500kHz, but doesn't seem to work for everybody. 
 *	1.4
 *		Removed redundant functions.
 *		Fixed mode check to include two other possible modes the controller could be in.
@@ -51,12 +51,12 @@
 *	1.6
 *		Changed config_gamepad() call to include rumble and pressures options
 *			This was to fix controllers that will only go into config mode once
-*			Old methods should still work for backwards compatibility
+*			Old methods should still work for backwards compatibility 
 *    1.7
 *		Integrated Kurt's fixes for the interrupts messing with servo signals
 *		Reorganized directory so examples show up in Arduino IDE menu
 *    1.8
-*		Added Arduino 1.0 compatibility.
+*		Added Arduino 1.0 compatibility. 
 *
 *
 *
@@ -66,7 +66,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 <http://www.gnu.org/licenses/>
-*
+*  
 ******************************************************************/
 
 
